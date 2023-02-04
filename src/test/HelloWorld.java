f206b5d94bbd80aadac4456040af502db029e7b7
ShoonLai <shunelai132@gmail.com>
2023-02-04 17:03:14 +0630

    added shoppingcart

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -9,6 +9,8 @@ public class HelloWorld {
 		System.out.println("Test vi editor");
 		
 		System.out.println("Inside developer_001 branch");
+		
+		System.out.println("to test shopping cart feature");
 
 	}
 
